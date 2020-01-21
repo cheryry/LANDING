c805e06501a5b2fa3f7e8bf9e11119c6da668dd6
cherdyy <maxim123456789101112@gmail.com>
2020-01-22 00:59:08 +0200

    ts lint fix, header add background on scroll, unsub service

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,20 +1,51 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { UnsubscribeService } from '@shared/services/unsubscribe.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [UnsubscribeService]
 })
+
 export class LandingComponent implements OnInit {
 
   public currentDate: Date;
 
-  constructor() {
+  @ViewChild('header', {static: true})
+  private header: ElementRef;
+
+  constructor(private unsubscribe$: UnsubscribeService) {
     this.currentDate = new Date();
   }
 
   ngOnInit() {
+    fromEvent<Event>(document, 'scroll')
+      .pipe(
+        takeUntil(this.unsubscribe$),
+      )
+      .subscribe(() => this.toggleHeaderScrolled());
   }
 
+  private toggleHeaderScrolled(): void {
+    const scrollTop = document.documentElement.scrollTop;
+
+    if(scrollTop === 0) {
+
+      if(this.checkHeaderScrolledClass)
+        this.header.nativeElement.classList.remove('scrolled');
+
+    } else {
+
+      if(!this.checkHeaderScrolledClass)
+        this.header.nativeElement.classList.add('scrolled');
+    }
+  }
+
+  private get checkHeaderScrolledClass(): boolean {
+    return this.header.nativeElement.classList.contains('scrolled');
+  }
 }
